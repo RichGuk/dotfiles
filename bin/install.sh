6ab57e9373d820374e11c8b331c2815f1be7fae1
Rich Grundy <rich@27smiles.com>
2012-06-07 11:25:35 +0100

    Use $HOME variable.

--- bin/install.sh
+++ bin/install.sh
@@ -2,7 +2,6 @@
 
 BIN_DIR=$(cd "$(dirname "$0")"; pwd)
 DOTFILES_DIR=$(dirname "$BIN_DIR")
-HOME_DIR=$(cd ~; pwd)
 
 symlinks=$(find $DOTFILES_DIR -name '*.symlink' -print)
 for symlink in $symlinks; do
@@ -13,18 +12,18 @@ for symlink in $symlinks; do
   basedir=${basedir#$DOTFILES_DIR} # Note: includes slash at the beginning.
 
   # Create the required directory structure if one is needed.
-  if [ ! -z "$basedir" ] && [ ! -e "${HOME_DIR}${basedir}" ]; then
-    $(mkdir -p "${HOME_DIR}${basedir}")
-    echo "Created directory: ${HOME_DIR}${basedir}"
+  if [ ! -z "$basedir" ] && [ ! -e "${HOME}${basedir}" ]; then
+    $(mkdir -p "${HOME}${basedir}")
+    echo "Created directory: ${HOME}${basedir}"
   fi
 
   # Create symlinks.
   # This will produce something like:
   # ln -fs /home/rich/.dotfiles/vimrc.symlink /home/rich/.vimrc
   if [ -z "$basedir" ]; then
-    link_at="${HOME_DIR}/${name}"
+    link_at="${HOME}/${name}"
   else
-    link_at="${HOME_DIR}${basedir}/${name}"
+    link_at="${HOME}${basedir}/${name}"
   fi
 
   # We need to manually unlink directories otherwise it'll create symlink within
