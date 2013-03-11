ec232a4a9c5581fc5832117dbc3362f611b523c6
Rich Grundy <rich@27smiles.com>
2013-03-11 09:27:10 +0000

    Added some aliases for running foreman.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,6 +12,13 @@ alias be='bundle exec'
 alias rspec='nocorrect rspec'
 alias vim='nocorrect vim'
 
+# Foreman for apps.
+alias fr_dev='bundle exec foreman run -e .env_development'
+alias fr_prod='bundle exec foreman run -e .env_production'
+alias fr_staging='bundle exec foreman run -e .env_staging'
+alias fr_test='bundle exec foreman run -e .env_test'
+alias fr=fr_dev
+
 alias -g with_tmux=' -t tmux a'
 
 alias todo='~/Scripts/todo.txt-cli/todo.sh -d ~/.todo.cfg'
