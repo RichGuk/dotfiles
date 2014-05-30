5926768c027f6c4399b444987b00ecede24eab86
Rich Grundy <rich@stinkyink.com>
2014-05-30 10:06:46 +0100

    Prompt update!!
    http://cl.ly/image/2k0P3a3g0o0r

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,14 @@
 setopt prompt_subst
 setopt auto_name_dirs
 
+PROMPT='${SSH_TTY:+[%n@%m] }%F{071}%~> %f'
+RPROMPT='%{$reset_color%}$(git_prompt_info)%{$reset_color%} '
 
-PROMPT='%(!.%F{red}(%n) .%F{magenta})%m%{$reset_color%} %{$fg[yellow]%}%~%{$reset_color%}$(git_prompt_info)%{$reset_color%}: '
+function ls_colours() {
+  for code in {000..255}; do print -P -- "$code: %F{$code}Test%f"; done
+}
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" %{$fg[yellow]%}(%{$reset_color%}%{$fg[green]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}%{$fg[yellow]%})"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}⚡%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%F{226} "
+ZSH_THEME_GIT_PROMPT_SUFFIX="%f"
+ZSH_THEME_GIT_PROMPT_DIRTY="⚡ "
 ZSH_THEME_GIT_PROMPT_CLEAN=""
