db47d5eebc2881f6eca6f66ae55d3932171f5c25
Richard Grundy <rich@striker.local>
2014-10-17 15:34:37 +0100

    Trying Brewfile
    Although looks like it'll be replaced at some point, keep an eye on it:
    
    https://github.com/Homebrew/homebrew/issues/30815
    https://github.com/Homebrew/homebrew/pull/30749

---
+++ Brewfile
@@ -0,0 +1,30 @@
+update
+
+install caskroom/cask/brew-cask
+
+# Core.
+install git
+install wget
+install vim --override-system-vi
+install tmux
+
+# Dev stuff.
+install mysql
+install postgres
+install memcached
+
+# Ruby related.
+install rbenv
+install ruby-build
+install rbenv-gem-rehash
+install rbenv-binstubs
+
+
+#
+# Applications.
+#
+cask install google-chrome
+cask install alfred
+
+cask install virtualbox
+cask install vagrant
--- bin/brew_setup.sh
+++ bin/brew_setup.sh
@@ -10,40 +10,8 @@ else
   brew update
 fi
 
-git version | grep Apple
+brew bundle >/dev/null 2>&1
 if [[ $? == 0 ]]; then
-  brew install git
+  brew tap homebrew/boneyard
 fi
 
-which -s tmux
-if [[ $? != 0 ]]; then
-  brew install tmux
-fi
-
-which -s rbenv
-if [[ $? != 0 ]]; then
-  brew install rbenv
-  brew install ruby-build
-  brew install rbenv-gem-rehash
-  brew install rbenv-binstubs
-
-  rbenv install ${RUBY_VERSION}
-  rbenv global ${RUBY_VERSION}
-
-  [[ -s "$HOME/.rbenv" ]] && eval "$(rbenv init -)"
-
-  gem update --system
-  gem install bundler foreman rails --no-rdoc --no-ri
-fi
-
-which -s mysql
-if [[ $? != 0 ]]; then
-  brew install mysql
-  mysql.server start
-  mysql_secure_installation
-fi
-
-which -s postgres
-if [[ $? != 0 ]]; then
-  brew install postgres
-fi
