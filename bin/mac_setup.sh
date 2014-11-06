03651122f2173d6baf3d50c6fad6a17d6112a384
Rich Grundy <rich@stinkyink.com>
2014-11-06 11:39:17 +0000

    Disable dashboard.

--- bin/mac_setup.sh
+++ bin/mac_setup.sh
@@ -189,6 +189,9 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0
 
+# Disable dashboard.
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
 
 #############################################################
 # Time Machine
