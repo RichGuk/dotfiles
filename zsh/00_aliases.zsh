cee164b27297e804acccd83df0833cec91ef24bf
Rich Grundy <rich@grundy.io>
2019-03-21 08:29:06 +0000

    Updated alias file.

--- zsh/00_aliases.zsh
+++ zsh/00_aliases.zsh
@@ -8,17 +8,10 @@ alias history='fc -l l'
 alias l='ls -la'
 alias ll='ls -l'
 alias lb='lsblk -o NAME,FSTYPE,SIZE,TYPE,LABEL,UUID,MOUNTPOINT,FSAVAIL,FSUSE%'
-
 alias be='bundle exec'
-
-alias -g with_tmux=' -t tmux a'
-
-alias todo='~/Scripts/todo.txt-cli/todo.sh -d ~/.todo.cfg'
-
-alias ealias="vim ${DOTFILES}/zsh/aliases.zsh"
-
+alias ealias="vim ${DOTFILES}/zsh/00_aliases.zsh"
 alias delmerged='git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch -d'
-
 alias _cp="rsync -aP"
+alias dco="PUID=$(id -u) PGID=$(id -g) nocorrect docker-compose"
 
 projects=$HOME/Projects
