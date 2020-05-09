a692157b9273bd6003dd5e6cf17642edb5b8a3f6
Rich Grundy <rich@grundy.io>
2020-05-09 09:32:25 +0100

    Some alias tweaks.

--- .config/zsh/00_aliases.zsh
+++ .config/zsh/00_aliases.zsh
@@ -11,11 +11,13 @@ alias lb='lsblk -o NAME,UUID,FSTYPE,SIZE,TYPE,LABEL,MOUNTPOINT,FSAVAIL,FSUSE%,MO
 alias be='bundle exec'
 alias ealias="vim ~/.config/zsh/00_aliases.zsh"
 alias delmerged='git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch -d'
-alias _cp="rsync -aP"
 alias dco="PUID=$(id -u) PGID=$(id -g) nocorrect docker-compose"
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
+
+alias vim="nvim"
+alias vi="nvim"
 
 
 projects=$HOME/Projects
