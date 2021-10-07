8681653ff6d709095a7909b5deca83d53998ebae
Rich Grundy <rich@grundy.io>
2021-10-07 09:22:03 +0100

    Some config tweaks.

--- .config/zsh/00_aliases.zsh
+++ .config/zsh/00_aliases.zsh
@@ -11,7 +11,6 @@ alias lb='lsblk -o NAME,UUID,FSTYPE,SIZE,TYPE,LABEL,MOUNTPOINT,FSAVAIL,FSUSE%,MO
 alias be='bundle exec'
 alias ealias="vim ~/.config/zsh/00_aliases.zsh"
 alias delmerged='git branch --merged | grep -v "\*" | grep -v master | grep -v develop | xargs -n 1 git branch -d'
-alias dco="PUID=$(id -u) PGID=$(id -g) nocorrect docker-compose"
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
