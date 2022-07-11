fa473353a7ffa9ac6d69c8513ad8a14b3d997639
Rich Grundy <rich@grundy.io>
2022-07-11 22:04:18 +0100

    Updated alias name.

--- .config/zsh/00_aliases.zsh
+++ .config/zsh/00_aliases.zsh
@@ -11,7 +11,7 @@ alias lb='lsblk -o NAME,UUID,FSTYPE,SIZE,TYPE,LABEL,MOUNTPOINT,FSAVAIL,FSUSE%,MO
 alias be='bundle exec'
 alias ealias="vim ~/.config/zsh/00_aliases.zsh"
 
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
 
 alias vim="nvim"
