c0c40289fcd67eff73a8be4775a221f8abd72c36
Rich Grundy <rich@grundy.io>
2023-05-12 23:44:42 +0100

    Wezterm update.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -22,32 +22,39 @@ local scheme = wezterm.get_builtin_color_schemes()['Snazzy']
 -- scheme.background = '#16222f'
 scheme.background = '#031926'
 
+local schemec = wezterm.get_builtin_color_schemes()['Catppuccin Mocha']
+-- scheme.background = '#16222f'
+schemec.background = '#171622'
+
 config.color_schemes = {
   ['Snazzy'] = scheme,
+  ['Catppuccin Mocha'] = schemec,
 }
-config.color_scheme = 'Snazzy'
+-- config.color_scheme = 'Snazzy'
+
+config.color_scheme = 'Catppuccin Mocha'
 
 config.colors = {
   tab_bar = {
     active_tab = {
-      bg_color = '#131d29',
-      fg_color = '#446c9c',
+      bg_color = '#313244',
+      fg_color = '#9399b2',
       italic = false,
     },
     inactive_tab = {
-      bg_color = '#131d29',
-      fg_color = '#2a425e',
+      bg_color = '#181825',
+      fg_color = '#45475a',
     },
     inactive_tab_hover = {
-      bg_color = '#1a293b',
-      fg_color = '#2a425e',
+      bg_color = '#313244',
+      fg_color = '#9399b2',
       italic = true,
     },
   },
 }
 
 config.window_frame = {
-  active_titlebar_bg = '#15212e',
+  active_titlebar_bg = '#171622',
   inactive_titlebar_bg = '#16222f',
 }
 
