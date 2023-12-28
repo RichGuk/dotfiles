a4cd834da4c28a69305f09c8c8fe5a5d62fc1dce
Rich Grundy <rich@grundy.io>
2023-12-28 12:44:26 +0000

    Added Catppuccin zsh syntax highlighting as module.

--- .config/zsh/99_plugins.zsh
+++ .config/zsh/99_plugins.zsh
@@ -3,4 +3,4 @@ source $(dirname $0)/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # Enable highlighters
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
-source $(dirname $0)/catppuccin_mocha-zsh-syntax-highlighting.zsh
+source $(dirname $0)/catppuccin-zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh
