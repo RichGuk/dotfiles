50f847162fdbd19aa1fcaae346fcb38b8b249082
Rich Grundy <rich@grundy.io>
2024-01-04 20:40:09 +0000

    Just naming bindings before I remove ones I don't use.

--- .config/zsh/00_bindings.zsh
+++ .config/zsh/00_bindings.zsh
@@ -2,27 +2,27 @@
 # ctrl that so set them up too.
 bindkey -v
 
-bindkey "^[[H" beginning-of-line
-bindkey "^[[1~" end-of-line
+bindkey "^[[H" beginning-of-line # Home
+bindkey "^[[1~" end-of-line # End
+bindkey "^[[F"  end-of-line  # End
+bindkey "^[[4~" end-of-line # End
 bindkey "^A" beginning-of-line
-bindkey "^[[F"  end-of-line
-bindkey "^[[4~" end-of-line
 bindkey "^E" end-of-line
-bindkey "^[^[[C" forward-word
-bindkey "^[^[[D" backward-word
+bindkey "^[^[[C" forward-word # Ctrl + Right
+bindkey "^[^[[D" backward-word # Ctrl + Left
 
 # Make it so after a partial type of word pressing up and down searches history
 # for that word, i.e. git and up shows all git history - love this.
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-bindkey '^r' history-incremental-search-backward
-bindkey "^[[Z" reverse-menu-complete
+bindkey '^[[A' up-line-or-search # Up
+bindkey '^[[B' down-line-or-search # Down
+bindkey '^r' history-incremental-search-backward # Search history backwards with Ctrl + r
+bindkey "^[[Z" reverse-menu-complete # Shift + Tab
 bindkey ' ' magic-space
 
 # Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
+bindkey '^?' backward-delete-char # Delete
+bindkey "^[[3~" delete-char  # Delete
+bindkey "^[3;5~" delete-char # Fn + Delete
+bindkey "\e[3~" delete-char # Delete
 
 bindkey -s ^f "tmux-sessionizer\n"
