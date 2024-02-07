4f18302baa4808180c32ced466e579f0853dbdc3
Rich Grundy <rich@grundy.io>
2024-02-07 15:12:03 +0000

    Diagnostic icons

--- .config/nvim/lua/richguk/plugins/lspconfig.lua
+++ .config/nvim/lua/richguk/plugins/lspconfig.lua
@@ -31,8 +31,25 @@ return {
         }
       end
 
+      local signs = {
+        { name = "DiagnosticSignError", text = "" },
+        { name = "DiagnosticSignWarn", text = "" },
+        { name = "DiagnosticSignHint", text = "" },
+        { name = "DiagnosticSignInfo", text = "" },
+      }
+
+      for _, sign in ipairs(signs) do
+        vim.fn.sign_define(
+          sign.name,
+          { text = sign.text, texthl = sign.name, linehl = "", numhl = "" }
+        )
+      end
+
       vim.diagnostic.config({
         virtual_text = false,
+        signs = {
+          active = signs,
+        },
         float = {
           header = false,
           border = "rounded",
