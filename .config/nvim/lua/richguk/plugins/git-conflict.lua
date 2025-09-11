4aea9718d80972eae9f3e1dee95d490c5a1c9f05
Rich Grundy <rich@grundy.io>
2025-09-11 10:18:26 +0100

    git-conflict doesn't look maintained and has deprecation warnings.

--- .config/nvim/lua/richguk/plugins/git-conflict.lua
+++ .config/nvim/lua/richguk/plugins/git-conflict.lua
@@ -1,9 +1,17 @@
+-- return {
+--   "akinsho/git-conflict.nvim",
+--   version = "*",
+--   config = function()
+--     require("git-conflict").setup({
+--       disable_diagnostics = true,
+--     })
+--   end,
+-- }
+--
+
 return {
-  "akinsho/git-conflict.nvim",
-  version = "*",
+  "sindrets/diffview.nvim",
   config = function()
-    require("git-conflict").setup({
-      disable_diagnostics = true,
-    })
+    require("diffview").setup({})
   end,
 }
