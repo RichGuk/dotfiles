83a48fe0d56326d2e9980784fe0283b0f37518db
Rich Grundy <rich@grundy.io>
2025-09-22 11:17:31 +0100

    ZSH changes.

--- .config/zsh/00_aliases.zsh
+++ .config/zsh/00_aliases.zsh
@@ -3,8 +3,10 @@ alias po="popd"
 
 # alias history='fc -l l'
 
-alias l='ls -la'
-alias ll='ls -l'
+alias ls='eza -l --icons=always'
+alias ll='eza -al --icons=always'
+alias lt='eza -a --tree --level=1 --icons=always'
+
 alias lb='lsblk -o NAME,UUID,FSTYPE,SIZE,TYPE,LABEL,MOUNTPOINT,FSAVAIL,FSUSE%,MODEL,SERIAL'
 alias be='bundle exec'
 alias ealias="vim ~/.config/zsh/00_aliases.zsh"
--- .config/zsh/00_colours.zsh
+++
@@ -1,3 +0,0 @@
-autoload -U colors && colors
-export LSCOLORS="Gxfxcxdxbxegedabagacad"
-ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
