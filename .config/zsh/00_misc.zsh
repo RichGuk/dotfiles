7fa49e8d65b9e22790960f576b2d0f6d7c649c67
Rich Grundy <rich@grundy.io>
2026-08-30 15:23:22 +0100

    Moving tmux/herdr to ctrl + s for prefix.

--- .config/zsh/00_misc.zsh
+++ .config/zsh/00_misc.zsh
@@ -4,3 +4,5 @@ setopt no_beep
 setopt auto_cd # cd to directory without typing cd
 setopt auto_pushd # pushd when cd
 setopt pushd_ignore_dups # ignore duplicated directory when pushd
+
+[[ -t 0 ]] && stty -ixon # ctrl+s is the herdr prefix, not XOFF
