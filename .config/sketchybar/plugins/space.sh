c43efde8824522fd5f64a56961736782edbf9aaa
Rich Grundy <rich@grundy.io>
2026-09-03 23:57:36 +0100

    Tweaks to Yabai/Aerospace.

--- .config/sketchybar/plugins/space.sh
+++ .config/sketchybar/plugins/space.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on
-else
-  sketchybar --set "$NAME" background.drawing=off
-fi
+# Yabai
+sketchybar --set "$NAME" background.drawing="$SELECTED"
+
+# AeroSpace
+# if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
+#   sketchybar --set $NAME background.drawing=on
+# else
+#   sketchybar --set $NAME background.drawing=off
+# fi
