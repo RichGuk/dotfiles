5fb83c44516e940ed92564ec00da0b620a9ec9c5
Rich Grundy <rich@grundy.io>
2026-09-04 13:06:16 +0100

    Track diffview plugin spec

---
+++ .config/nvim/lua/richguk/plugins/diffview.lua
@@ -0,0 +1,25 @@
+return {
+  "sindrets/diffview.nvim",
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
+  config = function()
+    local actions = require("diffview.actions")
+
+    require("diffview").setup({
+      view = {
+        default = {
+          disable_diagnostics = true,
+        },
+        merge_tool = {
+          layout = "diff3_mixed",
+        },
+      },
+      keymaps = {
+        view = {
+          { "n", "co",  actions.conflict_choose("ours"),        { desc = "Choose the OURS version of a conflict" } },
+          { "n", "ct",  actions.conflict_choose("theirs"),      { desc = "Choose the THEIRS version of a conflict" } },
+          { "n", "ce",  actions.conflict_choose("base"),        { desc = "Choose the BASE version of a conflict" } },
+        },
+      },
+    })
+  end,
+}
